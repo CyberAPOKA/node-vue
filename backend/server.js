3f6e0c2872e95f2fbe2e74b671f87ae942af26e1
Christian <oficialsteffens@hotmail.com>
2023-12-02 09:37:45 -0300

    sistema quase finalizado.

--- backend/server.js
+++ backend/server.js
@@ -45,18 +45,20 @@ db.connect((err) => {
 
 app.post('/persistnotes', async (req, res) => {
     const notes = req.body;
+    const token = generateToken(); // Gera um token único
 
     try {
         for (const note of notes) {
-            let sql = 'INSERT INTO notes SET ?';
-            await queryAsync(sql, note);
+            let sql = 'INSERT INTO notes SET ?, token = ?';
+            await queryAsync(sql, [note, token]);
         }
-        res.send('Notas salvas com sucesso');
+        res.json({ message: 'Notas salvas com sucesso', token: token });
     } catch (err) {
         console.error(err);
         res.status(500).send('Erro ao salvar notas no banco de dados');
     }
 });
+
 
 function queryAsync(sql, params) {
     return new Promise((resolve, reject) => {
@@ -66,3 +68,44 @@ function queryAsync(sql, params) {
         });
     });
 }
+
+// Era usado inicialmente para recuperar todos as anotações do banco de dados mysql.
+// app.get('/getnotes', async (req, res) => {
+//     try {
+//         let sql = 'SELECT * FROM notes';
+//         const notes = await queryAsync(sql);
+//         res.json(notes);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Erro ao recuperar notas do banco de dados');
+//     }
+// });
+
+app.delete('/deletenote/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        let sql = 'DELETE FROM notes WHERE id = ?';
+        await queryAsync(sql, [id]);
+        res.send('Nota excluída com sucesso');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao excluir nota do banco de dados');
+    }
+});
+
+app.get('/getnote/:token', async (req, res) => {
+    const { token } = req.params;
+    try {
+        let sql = 'SELECT * FROM notes WHERE token = ?';
+        const notes = await queryAsync(sql, [token]);
+        if (notes.length > 0) {
+            res.json(notes);
+        } else {
+            res.status(404).send('Nenhuma nota encontrada para este token');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao recuperar notas do banco de dados');
+    }
+});
+
